2bb2f856494de6ed8e38ec8010b2f5855076a6f2
elijah-ug <elijahtalentmugisha14@gmail.com>
2026-04-16 20:12:09 +0300

    changed cookie settings

--- api/src/controllers/auth/userLoginController.ts
+++ api/src/controllers/auth/userLoginController.ts
@@ -9,17 +9,18 @@ export const userLoginController = async (req: Request, res: Response) => {
 
     // Set HTTP-only cookies
     // process.env.NODE_ENV === 'production'
+    const isProduction = process.env.NODE_ENV === 'production';
     res.cookie('accessToken', result.accessToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      httpOnly: true, 
+      secure: isProduction,
+      sameSite: isProduction? 'none':  'lax',
       maxAge: 60 * 60 * 1000, // 1 hour
     });
 
     res.cookie('refreshToken', result.refreshToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      secure: isProduction,
+      sameSite: isProduction? 'none':  'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
