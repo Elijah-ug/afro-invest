871131000ad024c9de75db9cd574e302291796e1
Elijah-ug <elicomelijah330@gmail.com>
2026-04-25 19:15:21 +0300

    worked on the admin dashboard

--- api/src/services/withdrawReqServive.ts
+++ api/src/services/withdrawReqServive.ts
@@ -1,13 +1,10 @@
 import { prisma } from '../lib/prisma';
 
 export const withdrawRequest = async (data: any, id: number) => {
-  if (!data || !data.amount) {
-    throw new Error('Amount is required');
-  }
   return await prisma.withdrawRequest.create({
     data: {
       userId: id,
-      amout: data.amount,
+      amout: data.amount as number,
     },
   });
 };
